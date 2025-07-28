c6bba7542243284f3b3de3e38bd1eebb936d43be
Allysonhd <166686180+allysonhd@users.noreply.github.com>
2025-07-28 11:10:34 -0500

    created in-memory users for learning purposes

--- src/main/java/com/allysonprojects/notes/security/SecurityConfig.java
+++ src/main/java/com/allysonprojects/notes/security/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -22,5 +26,27 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable);
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    //in-memory user detail storage
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        UserDetails user1 = User.withUsername("user1") //one way of doing this
+                .password("{noop}password1") //tells Spring Security not performing encryption
+                .roles("USER")
+                .build();
+        if (!manager.userExists("user1")) {
+            manager.createUser(user1);
+        }
+        if (!manager.userExists("admin")) {
+            manager.createUser(
+                    User.withUsername("admin") //more streamlined way of doing this
+                            .password("{noop}adminPass")
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+        return manager;
     }
 }
